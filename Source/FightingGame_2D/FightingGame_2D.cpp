193ef8870fa06dcc57116a893138cfa8f0d03165
Eric Miller <ericmiller901@gmail.com>
2021-01-25 19:42:23 -0600

    Test

--- Source/FightingGame_2D/FightingGame_2D.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FightingGame_2D.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FightingGame_2D, "FightingGame_2D" );
- 
